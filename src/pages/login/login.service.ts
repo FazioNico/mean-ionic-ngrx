32e1a855000d58b6f6985d1753b79d8abd38713e
FazioNico <contact@nicolasfazio.ch>
2018-01-04 17:55:52 +0100

    Fix: Handle error refact

--- src/pages/login/login.service.ts
+++ src/pages/login/login.service.ts
@@ -3,7 +3,7 @@
 * @Date:   17-04-2017
 * @Email:  contact@nicolasfazio.ch
  * @Last modified by:   webmaster-fazio
- * @Last modified time: 13-11-2017
+ * @Last modified time: 04-01-2018
 */
 
 import { Injectable, Inject } from '@angular/core';
@@ -52,6 +52,10 @@ export class LoginService extends HttpService {
   isAuth():Observable<ICurrentUserState>{
     this.path = this._isAuthUrl
     return this.get().map(res=> res.isAuth || {})
+    .catch(err => Observable.of({
+      error:err.error.error || err.error || err.error.message || err,
+      message:err.message||'Authentication failed!'
+    }))
   }
 
   doAuth(_creds:any) :Observable<HttpServerResponse> {
@@ -60,11 +64,20 @@ export class LoginService extends HttpService {
       email:_creds.email,
       password: _creds.password
     })
+    .catch(err => Observable.of({
+      error:err.error.error || err.error || err.error.message || err,
+      message:err.message||'Login authentication failed!'}
+    ))
   }
 
   doCreateUser(_payload:{email:string, password:string}):Observable<HttpServerResponse> {
     this.path = this._signUpUrl
     return this.post(_payload)
+    .catch(err => Observable.of({
+      error:err.error.error || err.error || err.error.message || err,
+      message:err.message||'Signup failed!'
+    }))
+
   }
 
   /* Token managers Methodes */
