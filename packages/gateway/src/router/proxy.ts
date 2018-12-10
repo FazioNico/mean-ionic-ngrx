6187b3d55add68f6e4736e59b82afa1b6996b546
FazioNico <contact@nicolasfazio.ch>
2018-12-10 16:20:14 +0100

    upd(gateway): fix proxy requester + add comments

--- packages/gateway/src/router/proxy.ts
+++ packages/gateway/src/router/proxy.ts
@@ -22,14 +22,12 @@ interface IRequestOptions {
  * @param repo Redis instance ready
  */
 const proxyRequester = (repo) => express.Router().use(async (req: any, res, next) => {
-  console.log('=====================');
-  console.log('PROXY REQUESTER', req.path);
   // extract desired url.path
   const path: string = req.url.split('/').filter(param => param !== '').splice(1, 1).join('/');
+  console.log('[INFO]: Proxy requester. Find service: ', req.url.split('/')[1]);
   // extract existing token
   const token: string = req.get('x-access-token') || req.get('authentication') || req.get('authorization') || undefined;
   const { serverConfig: { backendToken = null } = {}} = req;
-  console.log('repo find....', req.url.split('/')[1]);
   // find in repo existing service
   const repoFind = await repo.find(req.url.split('/')[1]).catch(err => err);
   if (!repoFind || repoFind instanceof Error)
@@ -41,14 +39,20 @@ const proxyRequester = (repo) => express.Router().use(async (req: any, res, next
     // extract port from result
     port = null
   } = repoFind || {};
-  const responseStatus = 200;
+  let responseStatus = 200;
+  // handle errors
   if (!host || !port)
     return next({code: 500, message: 'Service not have PORT or HOST' });
+  // build hostmane
   const hostname = (process.env.NODE_ENV === 'production')
     ? `${host}:${port}`
     : `http://${host}:${port}`;
-  console.log('request proxy to -->', hostname, req.method.toUpperCase());
-  const body = JSON.stringify(req.body);
+  console.log('[INFO]: Send request proxy to -->', hostname + '/api/v2/' + path, req.method.toUpperCase());
+  console.log('[INFO]: with token -->', (token || '').length > 10);
+  console.log('[INFO]: with backendToken -->', (backendToken || '').length > 10);
+  const body = JSON.stringify(req.body || {});
+  console.log('[INFO]: with body -->',  (body || '').length > 1);
+  // build request options
   const requestOptions: http.RequestOptions = {
     method: req.method.toUpperCase(),
     hostname: hostname.split('//')[1].split(':')[0],
@@ -61,23 +65,23 @@ const proxyRequester = (repo) => express.Router().use(async (req: any, res, next
       'Content-Length': body.length
     },
   };
+  // do request to services
   const responseProxy = await new Promise((resolve, reject) => {
     const request = http.request(requestOptions, (resp) => {
       let data = '';
       // A chunk of data has been recieved.
       resp.on('data', (chunk) => data += chunk);
       // The whole response has been received. Print out the result.
-      resp.on('end', () => resolve(JSON.parse(data || null)));
+      resp.on('end', () => {
+        resolve(JSON.parse(data || null));
+      });
     })
     .on('error', (err) => reject(err));
     request.write(body);
     request.end();
   }).catch(err => err);
-  console.log('-----------------');
-  console.log('For host:', hostname, );
-  console.log('For path:', '/' + path, );
-  // console.log('responseProxy-> ', responseProxy);
-  console.log('END OF PROXY REQUESTER');
+  // update responseStatus
+  if (responseProxy.status || responseProxy.code) responseStatus = responseProxy.status || responseProxy.code;
   return res.status(responseStatus).json(responseProxy);
 });
 
